14194432a41c281ae0a097c3d3b824aa06e9930b
augustuswm <gusmayo@gmail.com>
2024-06-14 10:51:25 -0500

    Use async_trait

--- v-api/src/hook/mod.rs
+++ v-api/src/hook/mod.rs
@@ -2,8 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use async_trait::async_trait;
 use newtype_uuid::TypedUuid;
-use std::{future::Future, pin::Pin, sync::Arc};
+use std::sync::Arc;
 use v_model::{UserId, UserProviderId};
 
 pub struct PostUserRegisterAction(pub(crate) Vec<Arc<dyn PostUserRegister>>);
@@ -13,10 +14,7 @@ impl PostUserRegisterAction {
     }
 }
 
-pub trait PostUserRegister: Send + Sync + 'static {
-    fn run(
-        &self,
-        user: &TypedUuid<UserId>,
-        provider: &TypedUuid<UserProviderId>,
-    ) -> Pin<Box<dyn Future<Output = ()> + Send + Sync + 'static>>;
+#[async_trait]
+pub trait PostUserRegister: Send + Sync {
+    async fn run(&self, user: &TypedUuid<UserId>, provider: &TypedUuid<UserProviderId>);
 }
