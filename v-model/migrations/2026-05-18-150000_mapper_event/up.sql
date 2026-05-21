ca59906faff49434f8ff6602a0c502b278a902f8
augustuswm <gusmayo@gmail.com>
2026-05-21 11:25:32 -0500

    Fix merge failure

--- v-model/migrations/2026-05-18-150000_mapper_event/up.sql
+++ v-model/migrations/2026-05-18-150000_mapper_event/up.sql
@@ -4,9 +4,6 @@ CREATE TABLE mapper_event (
   mapper_name VARCHAR NOT NULL,
   user_id UUID NOT NULL,
   rule JSONB NOT NULL,
-<<<<<<< HEAD
-  ephemeral BOOLEAN NOT NULL DEFAULT false,
-=======
->>>>>>> main
+  source VARCHAR NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
