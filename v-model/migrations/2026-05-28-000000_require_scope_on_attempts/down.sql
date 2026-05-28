676ca429ac5faf3839eee05c5a01d7a1376bc402
augustuswm <gusmayo@gmail.com>
2026-05-28 13:41:58 -0500

    Restrict migration to mlink

--- v-model/migrations/2026-05-28-000000_require_scope_on_attempts/down.sql
+++ v-model/migrations/2026-05-28-000000_require_scope_on_attempts/down.sql
@@ -1,5 +1,2 @@
-ALTER TABLE login_attempt ALTER COLUMN scope DROP NOT NULL;
-ALTER TABLE login_attempt ALTER COLUMN scope DROP DEFAULT;
-
 ALTER TABLE magic_link_attempt ALTER COLUMN scope DROP NOT NULL;
 ALTER TABLE magic_link_attempt ALTER COLUMN scope DROP DEFAULT;
--- v-model/migrations/2026-05-28-000000_require_scope_on_attempts/up.sql
+++ v-model/migrations/2026-05-28-000000_require_scope_on_attempts/up.sql
@@ -1,7 +1,3 @@
-UPDATE login_attempt SET scope = '' WHERE scope IS NULL;
-ALTER TABLE login_attempt ALTER COLUMN scope SET DEFAULT '';
-ALTER TABLE login_attempt ALTER COLUMN scope SET NOT NULL;
-
 UPDATE magic_link_attempt SET scope = '' WHERE scope IS NULL;
 ALTER TABLE magic_link_attempt ALTER COLUMN scope SET DEFAULT '';
 ALTER TABLE magic_link_attempt ALTER COLUMN scope SET NOT NULL;
