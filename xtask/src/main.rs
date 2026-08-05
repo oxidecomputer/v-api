c543dfa9535ea3a63213e2eda753bea95b7a89a5
Augustus Mayo <augustus@oxidecomputer.com>
2026-08-05 13:04:34 -0500

    Do not bump remix package (#536)

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -152,16 +152,7 @@ fn bump_on_pr_branch(
     bump_package_versions(root_path, &new_version)?;
 
     let commit_message = format!("Bump to v{}", new_version);
-    git_status(
-        root_path,
-        [
-            "add",
-            "Cargo.toml",
-            "Cargo.lock",
-            "remix-auth-vapi/package.json",
-            "remix-auth-vapi/package-lock.json",
-        ],
-    )?;
+    git_status(root_path, ["add", "Cargo.toml", "Cargo.lock"])?;
     git_status(root_path, ["commit", "-m", commit_message.as_str()])?;
     git_status(root_path, ["checkout", &original_branch])?;
 
@@ -201,20 +192,25 @@ fn bump_package_versions(root_path: &Path, version: &Version) -> Result<(), Stri
 }
 
 fn update_npm_package_version(root_path: &Path, version: &Version) -> Result<(), String> {
-    let package_dir = root_path.join("remix-auth-vapi");
-    println!("Running npm version to update remix-auth-vapi's package.json/package-lock.json...");
-    let status = Command::new("npm")
-        .args([
-            "version",
-            &version.to_string(),
-            "--no-git-tag-version",
-            "--allow-same-version",
-        ])
-        .current_dir(&package_dir)
-        .status()
-        .map_err(|e| format!("failed to run npm version: {}", e))?;
-    if !status.success() {
-        return Err("npm version failed".to_string());
+    // We currently do not version any npm packages. Notable remix-auth-vapi is not versioned along
+    // with the rest of the packages.
+    let packages: Vec<String> = vec![];
+    for package in packages {
+        let package_dir = root_path.join(&package);
+        println!("Running npm version to update {package}'s package.json/package-lock.json...");
+        let status = Command::new("npm")
+            .args([
+                "version",
+                &version.to_string(),
+                "--no-git-tag-version",
+                "--allow-same-version",
+            ])
+            .current_dir(&package_dir)
+            .status()
+            .map_err(|e| format!("failed to run npm version: {}", e))?;
+        if !status.success() {
+            return Err("npm version failed".to_string());
+        }
     }
     Ok(())
 }
