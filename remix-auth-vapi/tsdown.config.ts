212c93215dfae150f4c411428d3298f8df12650f
Peter Tripp <peter@oxidecomputer.com>
2026-09-03 16:15:04 -0400

    remix-auth-vapi: Remove CJS support. ESM only

--- remix-auth-vapi/tsdown.config.ts
+++ remix-auth-vapi/tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   clean: true,
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   dts: true,
   target: false,
 })
